f4fca9514dfed1dc475bdb3176cb6fb74c3ef6ab
devsunset <devsunset@gmail.com>
2021-07-30 12:19:16 +0900

    dev

--- src/components/main/header/header.ts
+++ src/components/main/header/header.ts
@@ -1,14 +1,12 @@
 import {Options, Vue} from 'vue-class-component';
 import Messages from './messages/messages.vue';
 import Notifications from './notifications/notifications.vue';
-import Languages from './languages/languages.vue';
 import User from './user/user.vue';
 
 @Options({
     components: {
         'messages-dropdown': Messages,
         'notifications-dropdown': Notifications,
-        'languages-dropdown': Languages,
         'user-dropdown': User
     }
 })
