e3ff6946191e0122084ccfa695b7dbba53b863ac
devsunset <devsunset@gmail.com>
2021-08-05 20:25:19 +0900

    apply vue-3-jwt-refresh-token

--- src/components/main/footer/footer.ts
+++
@@ -1,7 +0,0 @@
-import {Options, Vue} from 'vue-class-component';
-import {version} from '../../../../package.json';
-
-@Options({})
-export default class Footer extends Vue {
-    private version: string = version;
-}
