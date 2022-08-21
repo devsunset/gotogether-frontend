eba178cecb47406fe5e13b274d8866f6ff32971e
devsunset <devsunset@gmail.com>
2022-08-21 23:13:38 +0900

    dev

--- src/views/togetheredit.vue
+++ src/views/togetheredit.vue
@@ -397,6 +397,9 @@ export default {
 
                             // 지도에 클릭 이벤트를 등록합니다
                             // 지도를 클릭하면 마지막 파라미터로 넘어온 함수를 호출합니다
+
+                            var self = this;
+
                             kakao.maps.event.addListener(map, 'click', function(mouseEvent) {        
                                 // 클릭한 위도, 경도 정보를 가져옵니다 
                                 var latlng = mouseEvent.latLng; 
@@ -407,9 +410,8 @@ export default {
                                 message += '경도는 ' + latlng.getLng() + ' 입니다';
                                 console.log(message)
 
-                                alert(this.latitude)
-                                // this.latitude =  latlng.getLat();
-                                // this.longitude =  latlng.getLng();
+                                 self.latitude =  latlng.getLat();
+                                 self.longitude =  latlng.getLng();
                             });
                             ///////////////////////////////////////////////////////////////////
                             /*
