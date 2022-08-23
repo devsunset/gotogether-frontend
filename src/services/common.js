be32865012972fbd5fad5f07fb5f71c482b56af5
devsunset <devsunset@gmail.com>
2022-08-23 16:54:40 +0900

    dev

--- src/services/common.js
+++ src/services/common.js
@@ -5,10 +5,11 @@ class Common {
        alert("x")
       var convertReqData = {};
       for (const [key, value] of Object.entries(reqData)) {
+        var rvalue = ''
         if(value !==null && value !=''){
-          value = value.replace(/\"/g,'\'')
+          rvalue = value.replace(/"/g,'\'')
         }
-        convertReqData.push({key,value})
+        convertReqData.push({key,rvalue})
       }
 
       return JSON.stringify(convertReqData)
