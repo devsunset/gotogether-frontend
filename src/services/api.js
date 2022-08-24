5a404826ad1d9c0ac8ef8141bcdad4400081ccfd
devsunset <devsunset@gmail.com>
2022-08-24 09:44:41 +0900

    dev

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "/api",
+  // Prod
+  baseURL: "/api",
+
+  // Dev
   // baseURL: "http://localhost:8081/api",
-  baseURL: "http://193.123.252.22:8282/api",
+  // baseURL: "http://193.123.252.22:8282/api",
   headers: {
     "Content-Type": "application/json",
   },
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
+const path = require("path");
+
 module.exports = {
   devServer: {
     port: 8080
-  }
+  },
+
+  outputDir: path.resolve(__dirname, "../gotogether-backend/src/main/resources/static/")
 }
+
+
+
+
+
